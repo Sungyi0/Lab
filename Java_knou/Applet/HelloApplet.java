c6d5adf3bde1ed9936094f4d86f11c06dde927c0
Minjung <sungyi00@gmail.com>
2016-03-06 19:19:14 +0900

    Applet!

--- Java_knou/Applet/HelloApplet.java
+++ Java_knou/Applet/HelloApplet.java
@@ -2,7 +2,30 @@ import java.awt.*;
 import java.applet.*;
 
 public class HelloApplet extends Applet {
-	public void paint(Graphics g) {
-		g.drawString("Hello, Java Applet!", 20, 50);
+	TextArea ta = null;
+	public HelloApplet() {
+		setLayout(new BorderLayout());
+		ta = new TextArea();
+		add(ta, BorderLayout.CENTER);
+	}
+	
+	public void init() {
+		StringBuffer szMsg = new StringBuffer();
+		szMsg.append("init() is invoked.\n");
+		szMsg.append(getCodeBase().toString()).append("\n");
+		szMsg.append(getLocale().toString()).append("\n");
+		ta.append(szMsg.toString());
+	}
+	
+	public void start() {
+		ta.append("start() is invoked.\n");
+	}
+	
+	public void stop() {
+		System.out.println("stop() is invoked. \n");
+	}
+
+	public void destroy() {
+		System.out.println("destroy() is invoked.\n");
 	}
 }
