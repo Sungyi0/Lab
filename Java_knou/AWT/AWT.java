45c99006097ef6c869ddf94017be5bc2546c5eb2
Minjung <sungyi00@gmail.com>
2016-03-06 16:51:55 +0900

    AWT FileDialog

--- Java_knou/AWT/AWT.java
+++ Java_knou/AWT/AWT.java
@@ -1,19 +1,37 @@
 import java.awt.*;
 
 class MyFrame extends Frame {
+	private FileDialog fDialog = null;
 	public MyFrame(String title) {
-		super(title);
-		this.setSize(400, 300);
-		this.setVisible(true);
+	super(title);
+	this.setSize(400, 300);
+	this.setVisible(true);
 	}
 	
 	public void paint(Graphics g) {
-		g.drawString("Hello AWT", 150, 150);
+		super.paint(g);
+		if (fDialog != null) putMsg(g);
+	}
+	public void update(Graphics g) {
+		putMsg(g);
+	}
+	public void setDialog(FileDialog f) {
+		fDialog = f;
+	}
+	private void putMsg(Graphics g) {
+		g.drawString("Directory: " + fDialog.getDirectory(), 10, 50);
+		g.drawString("File: " + fDialog.getFile(), 10, 100);
 	}
 }
 
 public class AWT {
 	public static void main(String args[]) {
-		MyFrame myFrame = new MyFrame("Hello AWT");
+		MyFrame myFrame = new MyFrame("FileDialogTest");
+		FileDialog f = new FileDialog(myFrame, "Open", FileDialog.LOAD);
+		
+		f.setVisible(true);
+		myFrame.setDialog(f);
+		
+		myFrame.repaint();
 	}
 }
