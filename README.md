00a09db335b41065d7f0b3ffe7f8190dea3f2dec
Minjung <sungyi00@gmail.com>
2016-03-13 17:48:47 +0900

    a

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Algoritm
 알고리틈!
+
+Python..
+ python NAME.py
+Cpp..
+ g++ -std=c++11 -o OUTPUT target.cpp
+Java..
+ javac target.java
+ java target
+  
---
+++ knou/Algorithm/ex1.py
@@ -0,0 +1,7 @@
+""" knapsack.. capacity(M), item count(n), 
+ value (p1, p2, p3, p4) = (15, 20, 9, 14)
+ weight (w1, w2, w3, w4) = (3, 5, 3, 4)
+ 
+"""
+
+#begin..
